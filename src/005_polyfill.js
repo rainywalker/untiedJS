079e5baa644c6f3c07206b39ffec7f61d1b43bf4
bsidesoft <hika@bsidesoft.com>
2015-11-27 01:56:01 +0900

    Update 005_polyfill.js

--- src/005_polyfill.js
+++ src/005_polyfill.js
@@ -1,4 +1,4 @@
-(function(){
+(function(){ //ut prototype
 	var es5 = Object.defineProperty ? 1 : 0, r = {}, fn = Object.prototype,
 		imuutable = es5 ? function (){
 			var a = arguments, i = 0, j = a.length;
@@ -11,7 +11,17 @@
 	else fn.immutable = immutable;
 	fn = Array.prototype;
 	Object.prototype.immutable('slice', fn.slice, 'indexOf', fn.indexOf, 'join', fn.join);
-	String.prototype.immutable('isNumber', function(){return parseFloat(this) + '' == this;});
+	String.prototype.immutable(
+		'isNumber', function(){return parseFloat(this) + '' == this;},
+		'ex', (function(){
+			var r = /\[([^\]]+)\]/gi,
+				re = function(_0, _1){return (new Function('ut', 'return (' + _1 + ');')).call(self, ut);},
+				self;
+			return function(target){
+				return this.indexOf('[') == NONE ? v : (self = target || null, this.replace(r, re));
+			};
+		})()
+	);
 })();
 if(!String.prototype.trim) (function(){
 	var trim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
@@ -19,18 +29,20 @@ if(!String.prototype.trim) (function(){
 })();
 (function(){
 	var offset = +new Date, v, fn = Date.prototype;
-	if(!fn.toISOString) fn.immutable('toISOString', function(){
-		var v;
-		return this.getUTCFullYear() +
-			'-' + (v = '0' + (this.getUTCMonth() + 1)).substr(v.length - 2) +
-			'-' + (v = '0' + this.getUTCDate()).substr(v.length - 2) +
-			'T' + (v = '0' + this.getUTCHours()).substr(v.length - 2) +
-			':' + (v = '0' + this.getUTCMinutes()).substr(v.length - 2) +
-			':' + (v = '0' + this.getUTCSeconds()).substr(v.length - 2) +
-			'.' + (this.getUTCMilliseconds() / 1000).toFixed(3).slice(2, 5) +
-			'Z';
-	});
-	if(!fn.toJSON) fn.immutable('toJSON', Date.prototype.toISOString);
+	if(!fn.toISOString) fn.immutable(
+		'toISOString', v = function(){
+			var v;
+			return this.getUTCFullYear() +
+				'-' + (v = '0' + (this.getUTCMonth() + 1)).substr(v.length - 2) +
+				'-' + (v = '0' + this.getUTCDate()).substr(v.length - 2) +
+				'T' + (v = '0' + this.getUTCHours()).substr(v.length - 2) +
+				':' + (v = '0' + this.getUTCMinutes()).substr(v.length - 2) +
+				':' + (v = '0' + this.getUTCSeconds()).substr(v.length - 2) +
+				'.' + (this.getUTCMilliseconds() / 1000).toFixed(3).slice(2, 5) +
+				'Z';
+		},
+		'toJSON', v
+	);
 	if(!W['r' + v]){
 		v = 'equestAnimationFrame',
 		W['r' + v] = W['webkitR' + v] || W['mozR' + v] || W['msR' + v] || (Date.now ? function(v){
@@ -82,65 +94,65 @@ json = {
 	})()
 },
 if(!W['JSON']) W['JSON'] = json;
-log = (function(){
-	var div, init = function(){
-		if(document.getElementById('bsConsole')) return;
-		div = doc.createElement('div');
-		div.innerHTML = '<style>'+
-		'#bsConsole{font-family:arial;position:fixed;z-index:999999;background:#eee;bottom:0;left:0;right:0;height:200px}'+
-		'#bsConsoleTab{background:#ccc;height:20px}'+
-		'#bsConsoleTabConsole,#bsConsoleTabElement{font-size:11px;margin:2px 5px;padding:0 5px;float:left;border:1px solid #666}'+
-		'#bsConsoleView{font-size:10px;overflow-y:scroll;height:180px}'+
-		'#bsConsoleViewElement{word-break:break-all;word-wrap:break-word}'+
-		'.bsConsoleItem{font-size:11px;border:1px solid #000;padding:5px;margin:5px;float:left}'+
-		'</style>'+
-		'<div id="bsConsole">'+
-			'<div id="bsConsoleTab">'+
-				'<div id="bsConsoleTabConsole">Console</div><div id="bsConsoleTabElement">Element</div>'+
-			'</div>'+
-			'<div id="bsConsoleView">'+
-				'<div id="bsConsoleViewConsole"></div><div id="bsConsoleViewElement" style="display:none"></div>'+
-			'</div>'+
-		'</div>';
-		doc.body.appendChild(div.childNodes[0]),
-		doc.body.appendChild(div.childNodes[0]),
-		doc.getElementById('bsConsole').onclick = function(e){
-			switch(e.target.id){
-			case'bsConsoleTab':
-				e.target.style.height = e.target.style.height == '200px' ? '20px' : '200px';
-				break;
-			case'bsConsoleTabElement':
-				doc.getElementById('bsConsoleViewConsole').style.display = 'none';
-				doc.getElementById('bsConsoleViewElement').style.display = 'block';
-				doc.getElementById('bsConsoleViewElement').innerHTML = '<pre>' + 
-					('<html>\n' + doc.getElementsByTagName('html')[0].innerHTML + '\n</html>').replace(/[<]/g, '&lt;') + 
-					'</pre>';
-				break;
-			case'bsConsoleTabConsole':
-				doc.getElementById('bsConsoleViewConsole').style.display = 'block';
-				doc.getElementById('bsConsoleViewElement').style.display = 'none';
-			}
+if(doc){
+	log = (function(){
+		var div, r = /[<]/g, d = new Date, init = function(){
+			if(doc.getElementById('bsConsole')) return;
+			div = doc.createElement('div');
+			div.innerHTML = '<style>'+
+			'#bsConsole{font-family:arial;position:fixed;z-index:999999;background:#fff;bottom:0;left:0;right:0;height:200px}'+
+			'#bsConsoleTab{background:#999;height:20px;color:#fff}'+
+			'#bsConsoleTabConsole,#bsConsoleTabElement{font-size:11px;margin:0 10px;line-height:20px;float:left}'+
+			'#bsConsoleView{font-size:10px;overflow-y:scroll;height:180px}'+
+			'#bsConsoleViewElement{word-break:break-all;word-wrap:break-word}'+
+			'.bsConsoleItem{font-size:11px;border:1px solid #000;padding:5px;margin:0 5px;float:left}'+
+			'.bsConsoleTime{font-size:10px;padding:5px}'+
+			'</style>'+
+			'<div id="bsConsole">'+
+				'<div id="bsConsoleTab">'+
+					'<div id="bsConsoleTabConsole">Console</div><div id="bsConsoleTabElement">Element</div>'+
+				'</div>'+
+				'<div id="bsConsoleView">'+
+					'<div id="bsConsoleViewConsole"></div><div id="bsConsoleViewElement" style="display:none"></div>'+
+				'</div>'+
+			'</div>';
+			doc.body.appendChild(div.childNodes[0]),
+			doc.body.appendChild(div.childNodes[0]),
+			doc.getElementById('bsConsole').onclick = function(e){
+				e = e.target;
+				switch(e.id){
+				case'bsConsoleTab':e.style.height = e.style.height == '200px' ? '20px' : '200px'; break;
+				case'bsConsoleTabElement':
+					doc.getElementById('bsConsoleViewConsole').style.display = 'none';
+					doc.getElementById('bsConsoleViewElement').style.display = 'block';
+					doc.getElementById('bsConsoleViewElement').innerHTML = '<pre>' + 
+						('<html>\n' + doc.getElementsByTagName('html')[0].innerHTML + '\n</html>').replace(r, '&lt;') + 
+						'</pre>';
+					break;
+				case'bsConsoleTabConsole':
+					doc.getElementById('bsConsoleViewConsole').style.display = 'block';
+					doc.getElementById('bsConsoleViewElement').style.display = 'none';
+				}
+			};
 		};
-	};
-	return function(){
-		var a = arguments, i = 0, j = a.length, item, v;
-		init();
-		item = '<div style="clear:both">';
-		while(i < j){
-			v = a[i++];
-			if(v && typeof v == 'object') v = JSON.stringify(v); 
-			item += '<div class="bsConsoleItem">' + v + '</div>';
+		return function(){
+			var a = arguments, i = 0, j = a.length, v, item = '<div style="clear:both"><div class="bsConsoleTime">' + (d.setTime(Date.now()), d.toJSON()) + '</div>';
+			init();
+			while(i < j){
+				if((v = a[i++]) && typeof v == 'object') v = JSON.stringify(v); 
+				item += '<div class="bsConsoleItem">' + v + '</div>';
+			}
+			div.innerHTML = item + '</div>';
+			doc.getElementById('bsConsoleViewConsole').appendChild(div.childNodes[0]);
+		};
+	})();
+	if(!W['console']) W.console = {log:log};
+	(function(){
+		var t = 'abbr,article,aside,audio,bdi,canvas,data,datalist,details,figcaption,figure,footer,header,hgroup,mark,meter,nav,output,progress,section,summary,time,video'.split(','),
+			f = doc.createDocumentFragment(), i = t.length, j = 'createElement' in f;
+		while(i--){
+			doc.createElement(t[i]);
+			if(j) f.createElement(t[i]);
 		}
-		div.innerHTML = item + '</div>';
-		doc.getElementById('bsConsoleViewConsole').appendChild(div.childNodes[0]);
-	};
-})();
-if(!W['console']) W.console = {log:log};
-if(doc) (function(){
-	var t = 'abbr,article,aside,audio,bdi,canvas,data,datalist,details,figcaption,figure,footer,header,hgroup,mark,meter,nav,output,progress,section,summary,time,video'.split(','),
-		f = doc.createDocumentFragment(), i = t.length, j = 'createElement' in f;
-	while(i--){
-		doc.createElement(t[i]);
-		if(j) f.createElement(t[i]);
-	}
-})();
+	})();
+}
