e4184263ac23967f1393b27b121c7b3bb9b1a3e8
bsidesoft <hika@bsidesoft.com>
2015-11-27 01:55:38 +0900

    Update 000_start.js

--- src/000_start.js
+++ src/000_start.js
@@ -1,16 +1,24 @@
 //빌드시 미니파이하지 않는다.
 var ut = (function(W){
 "use strict"
-var NONE = -1,
-	STR = 'string', OBJ = 'object', FUN = 'function', 
-
-	doc = document,
-	docel = document.documentElement,
-	body = doc.createElement('body'),
-	head = doc.getElementsByTagName('head')[0],
-	
-	err, ex, debug, json, log, detect,
-	
-	
-	DATA = {app:{}, sys:{}},
-	ut;	
+var NONE = -1, STR = 'string', OBJ = 'object', FUN = 'function', 
+	doc = document, docel = document.documentElement, body = doc.createElement('body'), head = doc.getElementsByTagName('head')[0],
+	debug, err = (function(){
+		var isDebug;
+		debug = function(v){isDebug = v;};
+		return function(){
+			console.log(num, msg);
+			if(isDebug) throw new Error(arguments.join(','));
+		};
+	})(),
+	DATA = {app:{}, sys:{}}, app = DATA.app, sys = DATA.sys,
+	ut = function(v){
+		var a, i, j;
+		if(v && typeof v == OBJ) for(i in v) app[i] = v[i];
+		else{
+			a = arguments, i = 0, j = a.length;
+			while(i < j) app[a[i++]] = a[i++];
+		}
+	},
+	json, log, detect,
+	i18n, Dom;	
